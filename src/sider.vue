db80e462e1ca1383f9e17d7b9ff3107814550e56
VisionTan <tiling.tan@foxmail.com>
2019-11-07 14:09:25 +0800

    布局组件基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluSider"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
